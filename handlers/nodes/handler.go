d677407f571dbeeb69f258497e54e6a84c138455
Nicolas Surleraux <nsurleraux@gmail.com>
2018-06-14 19:46:26 +0200

    Let kinetik create droplets (2)

--- handlers/nodes/handler.go
+++ handlers/nodes/handler.go
@@ -2,10 +2,14 @@ package nodes
 
 import (
 	"encoding/json"
+	"io/ioutil"
+	"kinetik-server/control"
 	"kinetik-server/data"
 	"kinetik-server/logger"
 	"kinetik-server/models"
+	"kinetik-server/rand"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 
@@ -39,5 +43,33 @@ func UpdateNode(w http.ResponseWriter, r *http.Request) {
 		logger.StdLog.Println("Added node " + nodeIP)
 	} else {
 		logger.ErrLog.Println("Error while getting node : " + err.Error())
+	}
+}
+
+func StartDocker(w http.ResponseWriter, r *http.Request) {
+	body, _ := ioutil.ReadAll(r.Body)
+	err := control.StartDocker(string(body))
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+	} else {
+		w.WriteHeader(200)
+	}
+}
+
+func CreateNode(w http.ResponseWriter, r *http.Request) {
+
+	cfg, err := control.CreateKlerk(&control.KlerkCreationOption{
+		Name:              "klerk-" + rand.String(4),
+		Region:            "ams3",
+		Size:              "s-1vcpu-1gb",
+		Token:             os.Getenv("DO_TOKEN"),
+		SSHKeyFingerprint: control.ComputePublicFingerprint("/root/.ssh/id_rsa"),
+	})
+
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+	} else {
+		w.WriteHeader(201)
+		json.NewEncoder(w).Encode(cfg)
 	}
 }
